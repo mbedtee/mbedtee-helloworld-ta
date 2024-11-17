d277d7b17982cae51d7fbbbafc5b62008b6b66dc
kapa.xl <kapa.xl@outlook.com>
2024-11-17 12:56:23 +0800

    Initial commit

---
+++ Makefile
@@ -0,0 +1,32 @@
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) 2019 KapaXL (kapa.xl@outlook.com)
+#
+
+LD = $(CROSS_COMPILE)ld
+CC = $(CROSS_COMPILE)gcc
+STRIP = $(CROSS_COMPILE)strip
+
+LIB = $(shell $(CC) -print-file-name=libgcc.a) -L$(MBEDTEE_LIBC) -lc
+
+CFLAGS = -Wall -Werror -O2 -nostdlib -std=gnu99 ${ARCH_CFLAGS} -I$(MBEDTEE_INC) -I$(MBEDTEE_INC)/tee
+
+LDFLAGS = -EL -pie -nostdlib -z max-page-size=4096
+
+APP = mbedtee-helloworld-ta
+MOD = mbedtee-helloworld-temp
+
+SRCS = mbedtee-helloworld-ta.c
+OBJS = ${SRCS:.c=.o}
+
+all : ${MOD}.mk
+	${MAKE} -f ${MOD}.mk ${OBJS}
+	${LD} $(LDFLAGS) -o ${APP} ${OBJS} ${LIB}
+	${STRIP} -d -R .comment ${APP}
+
+${MOD}.mk : ${SRCS}
+	@cp -f Makefile $@ && chmod +w $@
+	@${CC} ${CFLAGS} -M ${SRCS} >> $@
+
+clean :
+	@rm -rf ${OBJS} ${MOD}.mk
---
+++ mbedtee-helloworld-ta.c
@@ -0,0 +1,50 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2019 KapaXL (kapa.xl@outlook.com)
+ *
+ * MbedTEE HelloWorld TA
+ */
+
+#include <mmap.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <tee_internal_api.h>
+
+TEE_Result TA_EXPORT TA_CreateEntryPoint(void)
+{
+	printf("HelloWorld - TA_CreateEntryPoint\n");
+	return TEE_SUCCESS;
+}
+
+void TA_EXPORT TA_DestroyEntryPoint(void)
+{
+	printf("HelloWorld - TA_DestroyEntryPoint\n");
+	return;
+}
+
+TEE_Result TA_EXPORT TA_OpenSessionEntryPoint(
+		uint32_t paramTypes,
+		TEE_Param params[4],
+		void **session)
+{
+	printf("HelloWorld - TA_OpenSessionEntryPoint\n");
+
+	return TEE_SUCCESS;
+}
+
+void TA_EXPORT TA_CloseSessionEntryPoint(void *session)
+{
+	printf("HelloWorld - TA_CloseSessionEntryPoint\n");
+}
+
+TEE_Result TA_EXPORT TA_InvokeCommandEntryPoint(
+		void *session, uint32_t cmd_id,
+		uint32_t paramtypes, TEE_Param params[4])
+{
+	printf("HelloWorld - TA_InvokeCommandEntryPoint\n");
+
+	printf("cmd %d types %d\n", (int)cmd_id, (int)paramtypes);
+
+	return TEE_SUCCESS;
+}
